2a3692ea5b309c10c67c1410dcf5554ad56aaaa0
Wolf Niklas <niklas.schellhoeh@gmail.com>
2020-04-17 00:00:00 +0200

    Corrected padding for main nav elements.

--- development/app.581aa3f0.js
+++ development/app.581aa3f0.js
@@ -270,7 +270,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "40171" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "38605" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
