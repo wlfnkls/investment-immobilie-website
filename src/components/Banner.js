ed832deb905b88929b89050d16bbf0c7efe82c28
Wolf Niklas <niklas.schellhoeh@gmail.com>
2020-04-30 14:27:56 +0200

    Initial project setup. Beginning of header implementation.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,8 +1,14 @@
+import imageCurved from '../images/handshake-header-image-curved.jpg';
+
+
 const Banner = () => {
   const template = `
     <div class="banner--text">
       <h1 class="h1">Investment Immobilie</h1>
       <h2 class="h2">Partner für Finanzierung und Investment Immobilie</h2>
+    </div>
+    <div class="banner--image">
+      <img src="${imageCurved}" />
     </div>
   `;
 
