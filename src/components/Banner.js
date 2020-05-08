5cf5a39e1738cf0f4e795fc1829cf2542fc010f6
Wolf Niklas <niklas.schellhoeh@gmail.com>
2020-05-08 23:18:22 +0200

    Refactored banner.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -3,17 +3,16 @@ import image from '../images/handshake-header-image.jpg';
 
 const Banner = (element) => {
   const template = `
-    <div class="banner--hero">
-      <img src="${image}" />
-    </div>
-    <div class="banner--text">
+    <div class="container">
       <h1 class="h1">${element.getAttribute('data-first-headline')}</h1>
-      <h2 class="h2">${element.getAttribute('data-second-headline')}</h2>
-      <button class="button button--primary mt-4">Kontakt</button>
-    </div>
-    <div class="banner--image">
-      <img src="${imageCurved}" />
-    </div>
+      <!-- <div class="banner--hero">
+        <img src="${image}" />
+      </div> -->
+      <h2 class="h2 mt-4">${element.getAttribute('data-second-headline')}</h2>
+      <div class="button-row">
+        <button class="button button--secondary mt-4">Kontakt</button>
+      </div>
+    </div>  
   `;
 
   return template;
