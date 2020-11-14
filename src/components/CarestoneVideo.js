d457911c373df52e4ba33de4a9f4abe799d28b8a
Wolf Niklas <niklas.schellhoeh@gmail.com>
2020-11-14 21:57:51 +0100

    Refactor play overlay.

--- src/components/CarestoneVideo.js
+++ src/components/CarestoneVideo.js
@@ -6,13 +6,11 @@ const CarestoneVideo = () => {
     <div class="container mb-5">
       <a href="https://player.vimeo.com/video/429228019" target="_blank">
         <div class="video-container">
-          <div class="play-icon-container">
-          <img src="${PlayIcon}" class="play-icon" />
-          </div>
+          <span class="play-icon-container" style="background-image: url(${PlayIcon})"> </span>
           <img class="video-image" src="${Image}" />
         </div>
       </a>
-      <caption>© 2020 Carestone Group GmbH</caption>
+      <p>© 2020 Carestone Group GmbH</p>
     </div>  
   `;
 
